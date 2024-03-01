e1376873a4a118673217e39ba4e3c9247e59d016
ohjiyeon3275 <ojy940716@gmail.com>
2024-03-01 19:20:59 +0900

    feat: simple websocket connection between server and client

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,19 @@
-hello;
+// reason why i didnt import from 'ws' is JS itself support WebSocket
+// https://developer.mozilla.org/ko/docs/Web/API/WebSocket
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("connected to browser : ws");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log(">>> message: ", message.data, "from the Server");
+});
+
+socket.addEventListener("close", () => {
+  console.log("close connection from server");
+});
+
+setTimeout(() => {
+  socket.send("good! how are you?");
+}, 100000);
--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(3000, () => console.log(`listening on 3000`));
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  socket.send("hi client! how are you?");
+  socket.on("close", () => console.log("disconnected from browser"));
+});
+
+server.listen(3000, () => console.log(`listening on 3000`));

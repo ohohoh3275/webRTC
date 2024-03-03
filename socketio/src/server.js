de1ffdce5fa5e653030eedfa8dee451b4a3dcd0d
ohjiyeon3275 <ojy940716@gmail.com>
2024-03-03 18:34:31 +0900

    feat(socketio): add socketio module and clean files to prepare

--- socketio/src/server.js
+++ socketio/src/server.js
@@ -1,43 +1,15 @@
 import express from "express";
 import http from "http";
-import WebSocket from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-
-  socket["nickname"] = "anonymous";
-
-  console.log("Connected");
-
-  socket.on("close", () => {
-    console.log("closed ws connection");
-  });
-
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>{
-          console.log("sended")
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        });
-      case "nickname":
-        socket["nickname"] = message.payload;
-    }
-  });
-});
 
 server.listen(3000, () => console.log(`listening on 3000`));
